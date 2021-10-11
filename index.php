d089af2d977f5c6bb1938bfbeeae54c3ad273837
Pietro <pietrospitella@gmail.com>
2021-10-11 13:55:44 +0200

    snacks from 3 to 5

--- index.php
+++ index.php
@@ -49,26 +49,104 @@
     //     echo '<h1>'.$matches[$i]['homeTeam'].' - '.$matches[$i]['visitTeam'].' | '.$matches[$i]['homePoints'].' - '.$matches[$i]['visitPoints'].'</h1>';
     // }
 
-    // ## Snack 2
+    ## Snack 2
 
-    // //Passare come parametri GET name, mail e age 
-    $name = $_GET['name'];
-    $mail = $_GET['mail'];
-    $age = $_GET['age'];
-    //e verificare (cercando i metodi che non conosciamo nella documentazione) che 'name' sia più lungo di 3 caratteri
-    //che mail contenga un punto e una chiocciola 
-    //e che age sia un numero
-    //Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” 
+    // // //Passare come parametri GET name, mail e age 
+    // $name = $_GET['name'];
+    // $mail = $_GET['mail'];
+    // $age = $_GET['age'];
+    // //e verificare (cercando i metodi che non conosciamo nella documentazione) che 'name' sia più lungo di 3 caratteri
+    // //che mail contenga un punto e una chiocciola 
+    // //e che age sia un numero
+    // //Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” 
     
-    if (strlen($name)>3 && strpos($mail,'.') && strpos($mail,'@') && is_numeric($age)){
-        echo 'Accesso ok';
-    }
-    else {
-        echo 'Accesso no';
-    }
+    // if (strlen($name)>3 && strpos($mail,'.') && strpos($mail,'@') && is_numeric($age)){
+    //     echo 'Accesso ok';
+    // }
+    // else {
+    //     echo 'Accesso no';
+    // }
 
+    ## Snack 3
 
+    // Creare un array di array. Ogni array figlio avrà come chiave una data in questo formato: DD-MM-YYYY es 01-01-2007 e come valore un array di post associati a quella data. Stampare ogni data con i relativi post.
 
+    // $posts = [
+
+    //     '10/01/2019' => [
+    //         [
+    //             'title' => 'Post 1',
+    //             'author' => 'Michele Papagni',
+    //             'text' => 'Testo post 1'
+    //         ],
+    //         [
+    //             'title' => 'Post 2',
+    //             'author' => 'Michele Papagni',
+    //             'text' => 'Testo post 2'
+    //         ],
+    //     ],
+    //     '10/02/2019' => [
+    //         [
+    //             'title' => 'Post 3',
+    //             'author' => 'Michele Papagni',
+    //             'text' => 'Testo post 3'
+    //         ]
+    //     ],
+    //     '15/05/2019' => [
+    //         [
+    //             'title' => 'Post 4',
+    //             'author' => 'Michele Papagni',
+    //             'text' => 'Testo post 4'
+    //         ],
+    //         [
+    //             'title' => 'Post 5',
+    //             'author' => 'Michele Papagni',
+    //             'text' => 'Testo post 5'
+    //         ],
+    //         [
+    //             'title' => 'Post 6',
+    //             'author' => 'Michele Papagni',
+    //             'text' => 'Testo post 6'
+    //         ]
+    //     ],
+    // ];
+
+    // foreach ($posts as $key => $day) {
+    //     echo '<h1>'.$key.'</h1><br>';
+    //     foreach ($day as $key => $singlePost) {
+    //         echo '<h2>'.$singlePost['title'].'</h2><br>';
+    //         echo '<h3>'.$singlePost['author'].'</h3><br>';
+    //         echo '<p>'.$singlePost['text'].'</p><br>';
+    //     }
+    // }
+
+    ## Snack 4
+
+    // // Creare un array con 15 numeri casuali (da 1 a 100), tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta
+
+    // $randomNums = [];
+    // $i=0;
+
+    // while (count($randomNums) < 15) { 
+    //     $newNum = rand(1,100);
+        
+    //     if (!in_array($newNum, $randomNums)) {
+    //         $randomNums[] = $newNum;
+    //     }
+    // }
+    // var_dump($randomNums);
+
+    ## Snack 5
+
+    // Prendere un paragrafo abbastanza lungo, contenente diverse frasi. Prendere il paragrafo e suddividerlo in tanti paragrafi. Ogni punto un nuovo paragrafo.
+
+    $paragraph = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur doloremque quibusdam, quam accusamus porro et nulla aperiam repellendus. Consequuntur ipsum dolorum earum eum obcaecati aliquid at, ratione maiores natus debitis?';
+
+    echo $paragraph;
+
+    $newPara = explode('.',$paragraph);
+
+    var_dump($newPara);
     ?>
 </body>
 </html>
